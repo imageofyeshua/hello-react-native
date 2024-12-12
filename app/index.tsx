63152cc272afe0d97518295a96c8e2e7ce4bb4d1
Daniel Park <intheimageofyeshua@gmail.com>
2024-12-12 12:17:29 +0900

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+
+export default function Index() {
+  return (
+    <StartGameScreen />
+  );
+}
